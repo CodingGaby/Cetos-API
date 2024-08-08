9322b8d77ae819db10c6b27e43cdfb36d0155fbb
CodingGaby <macma.cube1@gmail.com>
2024-08-08 11:27:25 -0600

    feat: module to make http request

--- index.cjs
+++ index.cjs
@@ -1,19 +1,49 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const axios = require("axios");
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware para parsear JSON
+// Middleware to parse JSON to JSObject
 app.use(express.json());
 
-// Ruta de prueba
-app.get('/', (req, res) => {
-  res.send('Everything is fine 👍');
+// function to make remote GET request
+const makeRemoteGet = async (param) => {
+  if (!param) {
+    try {
+      const response = await axios.get(process.env.MOCK_ENDPOINT);
+      return response.data;
+    } catch (error) {
+      console.error("Error getting BP:", error);
+      throw error;
+    }
+  }
+
+  const res = await axios.get(`${process.env.MOCK_ENDPOINT + param}`);
+  return res.data;
+};
+
+// Test Route
+app.get("/", async (req, res) => {
+  try {
+    const result = await makeRemoteGet();
+    res.send(result);
+  } catch (error) {
+    res.status(500).send("Error making remote request");
+  }
 });
 
+app.get("/bp/:id", async (req, res) => {
+  try {
+    const result = await makeRemoteGet(req.params.id);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send("Error making remote request");
+  }
+});
 // Iniciar servidor
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
