2d05bdeb88bcbde122931d5ef2b3e301ddac675f
CodingGaby <macma.cube1@gmail.com>
2024-08-08 22:43:50 -0600

    fix: Ignore ASC

--- functions/login.js
+++ functions/login.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
 const loginUser = async () => {
   axios.post(`${process.env.REMOTE_SERVER_ENDPOINT}/Login`, {
     "CompanyDB": "SBODemoUS",
     "Password": "pristine",
     "UserName": "manager"
-  })
+  }, { httpsAgent: agent })
     .then(function (response) {
       console.log(response);
     })
