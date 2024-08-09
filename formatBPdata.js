3cec4ea96c577bf4d9ce9af6fbbfe41c3ddbff66
montse-villa03 <montserratvilla0113@gmail.com>
2024-08-08 21:44:56 -0600

    Establish the format and structure of BP data

---
+++ formatBPdata.js
@@ -0,0 +1,28 @@
+const formatBPData = (data) => {
+    return {
+      Code: data.Code,
+      Name: data.Name,
+      ForeignName:  data.ForeignName,
+      Group: data.Group,
+      Currency: data.Currency,
+      FederalTaxID: data.FederalTaxID
+      Contact: {
+        Tel1: data.Tel1,
+        Tel2: data.Tel2,
+        MobilePhone: data.MobilePhone,
+        Fax: data.Fax,
+        EMail: data.EMail
+      },
+      WebSite: data.WebSite,
+      ShippingType: data.ShippingType,
+      Password: data.Password,
+      FactoringIndicator: data.FactoringIndicator,
+      BusinessPartnerProject: data.BusinessPartnerProject,
+      Industry: data.Industry,
+      TypeOfBusiness: data.TypeOfBusiness,
+      AliasName: data.AliasName
+    };
+  };
+  
+  export default formatBPData;
+  
--- index.cjs
+++ index.cjs
@@ -1,17 +1,37 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express from "express";
+import dotenv from "dotenv";
+import makeRemoteGet from "./functions/makeRequest.js";
+import formatBPData from "./formatBPdata.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware para parsear JSON
+// Middleware to parse JSON to JSObject
 app.use(express.json());
 
-// Ruta de prueba
-app.get('/', (req, res) => {
-  res.send('Everything is fine 👍');
+// Test Route
+// GET all BP's 
+app.get("/", async (req, res) => {
+  try {
+    const result = await makeRemoteGet();
+    result = result.map(formatBPData);//format application
+    res.send(result);
+  } catch (error) {
+    res.status(500).send("Error making remote request");
+  }
+});
+
+// GET BP's by id
+app.get("/bp/:id", async (req, res) => {
+  try {
+    const result = await makeRemoteGet(req.params.id);
+    result = formatBPData(result);//format application
+    res.send(result);
+  } catch (error) {
+    res.status(500).send("Error making remote request");
+  }
 });
 
 // Iniciar servidor
